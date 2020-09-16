7491737340b8aad90f19bc9247c77f519184e4ca
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-09-16 10:33:18 +0300

    UPDATE: make controller file global

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -1,7 +1,16 @@
 module.exports = (app) => {
   const customers = require('../controllers/customer.js');
+
   // Create a new Customer
-  app.post('/customers', customers.create);
+  app.post('/customers', (req, res, next) => {
+    // Validate request
+    if (!req.body) {
+      res.status(400).send({
+        message: 'Content can not be empty!',
+      });
+    }
+    customers.create(req, res, next);
+  });
 
   // Retrieve all Customers
   app.get('/customers', customers.findAll);
@@ -10,7 +19,15 @@ module.exports = (app) => {
   app.get('/customers/:customerId', customers.findOne);
 
   // Update a Customer with customerId
-  app.put('/customers/:customerId', customers.update);
+  app.put('/customers/:customerId', (req, res, next) => {
+    // Validate request
+    if (!req.body) {
+      res.status(400).send({
+        message: 'Content can not be empty!',
+      });
+    }
+    customers.update(req, res, next);
+  });
 
   // Delete a Customer with customerId
   app.delete('/customers/:customerId', customers.delete);
