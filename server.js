765664712b576dcbf0c25ebc1b2ef855d0e1e2ea
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-09-16 08:37:22 +0300

    UPDATE: Restful api with node express & mysql

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const customerRoutes = require('./app/routes/customer-routes');
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get('/', (req, res) => {
+  res.send('you are connected');
+});
+
+// set port, listen for requests
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
+
+customerRoutes(app);
