83d15b48e56aaa18dbff999abca21f332c7392cd
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-09-16 09:30:46 +0300

    DELETE: commit

--- app/config/db.js
+++ app/config/db.js
@@ -4,9 +4,3 @@ module.exports = {
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB,
 };
-// module.exports = {
-//   HOST: 'localhost',
-//   USER: 'root',
-//   PASSWORD: 'password',
-//   DB: 'mydb',
-// };
